f006d397541a17d695c8720b519e825244a913af
captmomo <wy4786@gmail.com>
2018-03-11 22:48:25 +0800

    testing

--- routes.py
+++ routes.py
@@ -8,6 +8,10 @@ import binascii
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 @app.route('/process', methods=['POST'])
 def process():
     input = request.json
